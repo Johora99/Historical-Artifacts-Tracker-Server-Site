c5f7c0e505bdd406f22c928e101c3eb987a3ee93
Johora99 <fatimatuzjohora1999@gmail.com>
2024-12-23 23:36:29 +0600

    liked by user

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/',async(req,res)=>{
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3oeok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,10 +28,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     
     const allArtifactsCollection = client.db('historicalArtifactsTracker').collection('allArtifacts');
-  
+    const likeCollection = client.db('historicalArtifactsTracker').collection('likeByUser');
     // get latest 6 artifacts data =============================
     app.get('/latestArtifacts',async(req,res)=>{
       const result = await allArtifactsCollection.find().sort({_id : -1}).limit(6).toArray();
@@ -39,11 +39,45 @@ async function run() {
     })
     // get all artifacts data =======================
    app.get('/allArtifacts',async(req,res)=>{
-    const result = await allArtifactsCollection.find().toArray();
+    const result = await allArtifactsCollection.find().sort({_id : -1}).toArray();
     res.send(result);
    })
-   
+  //  get artifact in the base of id ==============================
+  app.get('/allArtifacts/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await allArtifactsCollection.findOne(query);
+  
+    res.send(result)
+  })
+  //  post artifact in database ==========================
+  app.post('/allArtifacts',async(req,res)=>{
+    const newArtifact = req.body;
+    const result = await allArtifactsCollection.insertOne(newArtifact);
+    res.send(result);
+  })
 
+  // like by user ====================================
+  app.post('/like',async(req,res)=>{
+    const newLike = req.body;
+    const userEmail = newLike?.liked_by;
+    // const id = newLike.artifacts_Info?.id;
+    const artifact_id = newLike.artifacts_Info._id;
+    const filter = { 'liked_by': userEmail, 'artifacts_Info._id': artifact_id };
+    const findLike = await likeCollection.findOne(filter);
+    if(findLike){
+      console.log('already liked')
+      return ('already liked')
+    }
+    const query = {_id : new ObjectId(artifact_id)};
+    const result = await likeCollection.insertOne(newLike);
+    const update = {
+      $inc : {like_count : 1},
+    }
+    const updateLike = allArtifactsCollection.updateOne(query,update)
+    res.send(result);
+  })
+  
 
 
 
